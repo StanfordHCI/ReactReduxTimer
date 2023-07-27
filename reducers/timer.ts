4429dd2d157bbae8c602aad4d038fa30ed4f40eb
anisha7 <anishaj037@gmail.com>
2023-07-27 00:28:23 -0700

    Add reducer

--- reducers/timer.ts
+++ reducers/timer.ts
@@ -1,40 +1,46 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from '../store'
+import { TimeDelta } from '../helpers/TimeDelta'
 
 // Define a type for the slice state
 interface TimerState {
+    id: string,
     content: string,
     type: string,
     delta: TimeDelta,
-
+    finished: boolean,
+    pause: boolean,
+    createdTime: Date,
 }
 
 // Define the initial state using that type
-const initialState: CounterState = {
-  value: 0,
-}
+const initialState: Array<TimerState> = []
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const timerSlice = createSlice({
+  name: 'timer',
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1
+    add: (state, payload:any) => {
+      state.push(payload)
     },
-    decrement: (state) => {
-      state.value -= 1
+    update: (state, payload:any) => {
+      // TODO: state find by id and update with new data
     },
     // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
+    remove: (state, payload) => {
+      // TODO find by id in state and delete
     },
   },
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { add, update, remove } = timerSlice.actions
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectCount = (state: RootState) => state.counter.value
+export const getSortedTimers = (state: RootState) => state.sort((a: TimerState, b: TimerState) => {
+        return a.createdTime.getMilliseconds() - b.createdTime.getMilliseconds();
+    }).filter((element: TimerState) => {
+        return element.finished === false;
+    }).reverse();
 
-export default counterSlice.reducer
+export default timerSlice.reducer
